b1ff011e4d0e20737c609544e470ea5a11b891e8
Aaron France <afrance@6river.com>
2024-01-12 02:23:51 +0100

    actually run it now

--- alertmanager/init.sh
+++ alertmanager/init.sh
@@ -4,16 +4,12 @@ export SMTP_EMAIL_TO=$(cat /run/secrets/smtp_to)
 export SMTP_EMAIL_FROM=$(cat /run/secrets/smtp_from)
 export SMTP_SMART_HOST=$(cat /run/secrets/smtp_smarthost)
 export SMTP_USERNAME=$(cat /run/secrets/smtp_username)
-export SMTP_PASSWORD=$(cat /run/secrets/smtp_username)
+export SMTP_PASSWORD=$(cat /run/secrets/smtp_password)
 
 sed "s#\$SMTP_EMAIL_TO#${SMTP_EMAIL_TO}#g" /config/alertmanager.yml | \
     sed "s#\$SMTP_EMAIL_FROM#${SMTP_EMAIL_FROM}#g" | \
     sed "s#\$SMTP_SMART_HOST#${SMTP_SMART_HOST}#g" | \
     sed "s#\$SMTP_USERNAME#${SMTP_USERNAME}#g" | \
     sed "s#\$SMTP_PASSWORD#${SMTP_PASSWORD}#g" > /tmp/alertmanager.yml
-
-cat /tmp/alertmanager.yml
-
-sleep 3600
 
 /bin/alertmanager $@
