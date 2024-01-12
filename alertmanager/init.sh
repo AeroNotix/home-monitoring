670e33da6e1c0491e49623faf7f07d6ead8f295c
Aaron France <afrance@6river.com>
2024-01-12 02:23:11 +0100

    sed don't get down with @

--- alertmanager/init.sh
+++ alertmanager/init.sh
@@ -6,11 +6,11 @@ export SMTP_SMART_HOST=$(cat /run/secrets/smtp_smarthost)
 export SMTP_USERNAME=$(cat /run/secrets/smtp_username)
 export SMTP_PASSWORD=$(cat /run/secrets/smtp_username)
 
-sed "s@\$SMTP_EMAIL_TO@${SMTP_EMAIL_TO}@g" /config/alertmanager.yml | \
-    sed "s@\$SMTP_EMAIL_FROM@${SMTP_EMAIL_FROM}@g" | \
-    sed "s@\$SMTP_SMART_HOST@${SMTP_SMART_HOST}@g" | \
-    sed "s@\$SMTP_USERNAME@${SMTP_USERNAME}@g" | \
-    sed "s@\$SMTP_PASSWORD@${SMTP_PASSWORD}@g" > /tmp/alertmanager.yml
+sed "s#\$SMTP_EMAIL_TO#${SMTP_EMAIL_TO}#g" /config/alertmanager.yml | \
+    sed "s#\$SMTP_EMAIL_FROM#${SMTP_EMAIL_FROM}#g" | \
+    sed "s#\$SMTP_SMART_HOST#${SMTP_SMART_HOST}#g" | \
+    sed "s#\$SMTP_USERNAME#${SMTP_USERNAME}#g" | \
+    sed "s#\$SMTP_PASSWORD#${SMTP_PASSWORD}#g" > /tmp/alertmanager.yml
 
 cat /tmp/alertmanager.yml
 
