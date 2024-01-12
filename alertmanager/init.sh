d42e31096ecdb46b1b7acf8518dc23a5d55ef8f6
Aaron France <afrance@6river.com>
2024-01-12 01:29:01 +0100

    grab the secrets out the files

--- alertmanager/init.sh
+++ alertmanager/init.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+export SMTP_EMAIL_TO=$(</run/secrets/smtp_to)
+export SMTP_EMAIL_FROM=$(</run/secrets/smtp_from)
+export SMTP_SMART_HOST=$(</run/secrets/smtp_smarthost)
+export SMTP_USERNAME=$(</run/secrets/smtp_username)
+export SMTP_PASSWORD=$(</run/secrets/smtp_username)
+
 cat /config/alertmanager.yml | while IFS= read -r line; do
     echo $line
 done
 
+
+sleep 3600
 /bin/alertmanager $@
