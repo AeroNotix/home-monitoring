fedff9f275b891c3bb735b1a63cc344a92b5160d
Aaron France <afrance@6river.com>
2024-01-12 02:17:38 +0100

    just use fucking sed

--- alertmanager/init.sh
+++ alertmanager/init.sh
@@ -6,7 +6,7 @@ export SMTP_SMART_HOST=$(</run/secrets/smtp_smarthost)
 export SMTP_USERNAME=$(</run/secrets/smtp_username)
 export SMTP_PASSWORD=$(</run/secrets/smtp_username)
 
-sed "s@\$SMTP_EMAIL_TO@${SMTP_EMAIL_TO}@g" alertmanager.yml | \
+sed "s@\$SMTP_EMAIL_TO@${SMTP_EMAIL_TO}@g" /config/alertmanager.yml | \
     sed "s@\$SMTP_EMAIL_FROM@${SMTP_EMAIL_FROM}@g" | \
     sed "s@\$SMTP_SMART_HOST@${SMTP_SMART_HOST}@g" | \
     sed "s@\$SMTP_USERNAME@${SMTP_USERNAME}@g" | \
